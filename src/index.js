5e6fd001ab5b6f8f77ee60fd359b71dc39752076
nateos <sodjanathan@gmail.com>
2021-01-19 21:36:29 +0000

    Implementing Math.random

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-console.log('index.js')
+
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', () => {
+    // get random number btn 0 - 3, colorsIndex is not more than 3
+    const randomNumber = getRandomNumber()
+    console.log(randomNumber)
+
+    document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+})
+
+const getRandomNumber = () => {
+    return Math.random()
+}
